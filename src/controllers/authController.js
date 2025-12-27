69105e9f7c31b1bf87281b321b8f5dc9fc41962d
Yevheniia Serhieieva <y.o.serhieieva@gmail.com>
2025-12-27 14:55:24 +0200

    all

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,6 +3,8 @@ import bcrypt from 'bcrypt';
 import { User } from '../models/user.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
 import { Session } from '../models/session.js';
+import { sendEmail } from '../utils/sendMail.js';
+import jwt from 'jsonwebtoken';
 
 export const registerUser = async (req, res) => {
   const { userName, email, password } = req.body;
@@ -53,4 +55,35 @@ export const logoutUser = async (req, res) => {
   res.clearCookie('refreshToken');
 
   res.status(204).send();
+};
+
+export const requestResetEmail = async (req, res) => {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw createHttpError(404, 'User not found!');
+  }
+
+  const jwtToken = jwt.sign({ sub: user._id, email }, process.env.JWT_SECRET, {
+    expiresIn: '20m',
+  });
+  const link = `${process.env.FRONTEND_DOMAIN}/reset-password?token=${jwtToken}`;
+
+  try {
+    await sendEmail({
+      from: process.env.SMTP_FROM,
+      to: email,
+      subject: 'Reset your password',
+      html: `<p>Click <a href="${link}">here</a> to reset your password!</p>`,
+    });
+  } catch (error) {
+    console.error(error);
+    throw createHttpError(
+      500,
+      'Failed to send the email, please try again later.',
+    );
+  }
+
+  res.status(200).json({ message: 'Password reset email sent successfully' });
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,11 +3,13 @@ import { celebrate } from 'celebrate';
 import {
   loginUserShema,
   registerUserSchema,
+  requestResetEmailSchema,
 } from '../validation/authValidation.js';
 import {
   loginUser,
   registerUser,
   logoutUser,
+  requestResetEmail,
 } from '../controllers/authController.js';
 
 const router = Router();
@@ -15,4 +17,10 @@ const router = Router();
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserShema), loginUser);
 router.post('/auth/logout', logoutUser);
+
+router.post(
+  '/auth/request-reset-email',
+  celebrate(requestResetEmailSchema),
+  requestResetEmail,
+);
 export default router;
